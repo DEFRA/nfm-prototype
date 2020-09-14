9a5bbae9e3e70cd4abeefb127852d14982cb2180
Robin Parker <robinkristianparker@gmail.com>
2020-09-14 08:03:47 +0100

    Update config.js

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Finding funding for Natural Flood Management',
 
   // Default port that prototype runs on
   port: '3000',
